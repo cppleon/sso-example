b7b25002fc786c4ef9393cdb83c6eb3e4e149358
Aslan Gurbanov <cppleon@gmail.com>
2018-03-19 11:57:15 +0400

    sso

---
+++ hub/public/hub.js
@@ -0,0 +1,3 @@
+CrossStorageHub.init([
+  { origin: /127.0.0.1:420[12]$/, allow: ['get', 'set', 'del', 'getKeys', 'clear'] }
+]);
--- ui2/src/app/app.component.ts
+++ ui2/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const CrossStorageClient: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    const storage = new CrossStorageClient('http://localhost:3000');
+
+    setTimeout(() => {
+      storage.onConnect().then(() => {
+        return storage.getKeys();
+      }).then((keys) => {
+        console.log(keys);
+      });
+    }, 2000);
+  }
 }
